207dad783c797e141a4520edcf9799413ab36680
leia.hong <honglyan@outlook.com>
2017-06-20 02:56:14 -0700

    add student gongneng

---
+++ stu/stu.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <string.h>
+#include <mysql/mysql.h>
+#include "cgic.h"
+
+char * headname = "head.html";
+char * footname = "footer.html";
+char * stu = "stu.html";
+int cgiMain()
+{
+
+	fprintf(cgiOut, "Content-type:text/html;charset=utf-8\n\n");
+/*	fprintf(cgiOut, "<head><meta charset=\"utf-8\"/><title>查询结果</title>\
+			<style>table {width:400px; margin: 50px auto; border: 1px solid gray; border-collapse: collapse; border-spacing: none; text-align:center;}\
+			tr,td,th{border: 1px solid gray;}\
+			</style>\
+			</head>");*/
+	char ch;
+
+	FILE * fd;
+		if(!(fd = fopen(stu, "r"))){
+			fprintf(cgiOut, "Cannot open file, %s\n", stu);
+			return -1;
+		}
+		ch = fgetc(fd);
+
+		while(ch != EOF){
+			fprintf(cgiOut, "%c", ch);
+			ch = fgetc(fd);
+		}
+		fclose(fd);
+	return 0;
+}
